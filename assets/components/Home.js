302713e8d89afca89a645b044bff2fc650f3e079
Inki <aud.inki.dev@gmail.com>
2019-11-22 15:17:12 +0200

    coinfig webpack.config.js

---
+++ assets/components/Home.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {Route, Switch,Redirect, Link, withRouter} from 'react-router-dom';
+import Users from './Users';
+import Posts from './Posts';
+
+class Home extends Component {
+
+
+}
+
+export default Home;
--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Limenius\ReactBundle\LimeniusReactBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
